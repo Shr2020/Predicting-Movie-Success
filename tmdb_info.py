66a669a25fed2b6cf17144b03df256f0d42afa4d
Aanchal Khandelwal <aan2210khandelwal@gmail.com>
2022-04-17 00:36:45 -0400

    cast info tmdb

--- tmdb_info.py
+++ tmdb_info.py
@@ -18,9 +18,9 @@ for file in glob.glob(folderpath):
 gender_map = {1: 'Female', 2: 'Male'}
 movieobj = Movie()
 
-
 for file in files:
     budgetinfo = []
+    castinfo = []
     movies = pd.read_csv(file)
     for ind, movie in movies.iterrows():
         moviename = movie['Release Group']
@@ -29,15 +29,36 @@ for file in files:
             movieid = result[0]['id']
             details = movieobj.details(movieid)
             
-            info = {'movie': moviename, 'tmdbid': movieid, 
-            'adult': details['adult'], 'budget': details['budget'], 
-            'tmdb_popularity': details['popularity'], 'revenue': details['revenue'], 'runtime': details['runtime']}
-        # print(result)
-        else:
-            info = {'movie': moviename, 'tmdbid': '', 
-            'adult': '', 'budget': '', 
-            'tmdb_popularity': '', 'revenue': '', 'runtime': ''}
-        budgetinfo.append(info)
+            # info = {'movie': moviename, 'tmdbid': movieid, 
+            # 'adult': details['adult'], 'budget': details['budget'], 
+            # 'tmdb_popularity': details['popularity'], 'revenue': details['revenue'], 'runtime': details['runtime']}
 
-    df = pd.DataFrame(budgetinfo, columns=['movie', 'tmdbid', 'adult', 'budget', 'tmdb_popularity', 'revenue', 'runtime'])
-    df.to_csv('TMDB/tmdb_%s' % file.split('\\')[1], index=False)
+            cast = details['casts']['cast']
+            cast_info = {'movie': moviename, 'tmdbid': movieid, 'imdbid': details['imdb_id']}
+
+            for i in range(5):
+                if i < len(cast):
+                    cast_info['actor%s' % str(i+1)] = cast[i]['name']
+                    cast_info['actor%s_popularity' % str(i+1)] =  cast[i]['popularity']
+                else:
+                    cast_info['actor%s' % str(i+1)] = ''
+                    cast_info['actor%s_popularity' % str(i+1)] =  ''
+        else:
+            # info = {'movie': moviename, 'tmdbid': '', 
+            # 'adult': '', 'budget': '', 
+            # 'tmdb_popularity': '', 'revenue': '', 'runtime': ''}
+            cast_info = {'movie': moviename, 'tmdbid': '', 'imdbid': '', 
+                         'actor1': '', 'actor1_popularity': '',
+                         'actor2': '', 'actor2_popularity': '',
+                         'actor3': '', 'actor3_popularity': '',
+                         'actor4': '', 'actor4_popularity': '',
+                         'actor5': '', 'actor5_popularity': ''}
+        # budgetinfo.append(info)
+        castinfo.append(cast_info)
+
+    # df = pd.DataFrame(budgetinfo, columns=['movie', 'tmdbid', 'adult', 'budget', 'tmdb_popularity', 'revenue', 'runtime'])
+    # df.to_csv('TMDB/tmdb_%s' % file.split('\\')[1], index=False)
+
+    df = pd.DataFrame(castinfo, columns=['movie', 'tmdbid', 'imdbid', 'actor1', 'actor1_popularity', 'actor2', 
+    'actor2_popularity', 'actor3', 'actor3_popularity', 'actor4', 'actor4_popularity', 'actor5', 'actor5_popularity'])
+    df.to_csv('TMDB/tmdb_cast_%s' % file.split('\\')[1], index=False)
